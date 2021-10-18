eb02a3ea6209594070b32bce213a65a9af7c9954
Clearccl <1422491071@qq.com>
2021-10-18 16:28:29 +0800

    first commit

---
+++ src/main/java/com/example/mq_provider_war/MqProviderWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.mq_provider_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqProviderWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqProviderWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mq_provider_war/MqProviderWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mq_provider_war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqProviderWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
